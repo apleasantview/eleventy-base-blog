98f59127888baa4a7b7f3d29047b575dc6cb7b51
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-01-06 01:27:13 +0100

    Add required data files

---
+++ src/_data/site.js
@@ -0,0 +1,7 @@
+export default {
+  title: "Eleventy Base Blog (Baseline Edition)",
+  tagline: "Baseline + Eleventy Base Blog",
+  url: process.env.URL || "http://localhost:8080/",
+  defaultLanguage: "en",
+  noindex: false
+};
