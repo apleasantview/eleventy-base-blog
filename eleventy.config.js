215aa6e2a1e04471e55138c5723baa8395cae2e9
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-08-25 02:28:10 +0200

    Bring back the atom feed xsl and rss plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,7 @@ import settings from './src/_data/settings.js';
 
 import pluginFilters from "./_config/filters.js";
 import pluginNavigation from "@11ty/eleventy-navigation";
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default async function (eleventyConfig) {
@@ -27,6 +28,30 @@ export default async function (eleventyConfig) {
 	});
 
 	eleventyConfig.addPlugin(pluginNavigation);
+
+	eleventyConfig.addPassthroughCopy({ "./src/content/feed/pretty-atom-feed.xsl": "/feed/pretty-atom-feed.xsl" });
+	eleventyConfig.addPlugin(feedPlugin, {
+		type: "atom", // or "rss", "json"
+		outputPath: "/feed/feed.xml",
+		stylesheet: "pretty-atom-feed.xsl",
+		templateData: {
+			eleventyNavigation: {
+				key: "Feed",
+				order: 4
+			}
+		},
+		collection: {
+			name: "posts",
+			limit: 10,
+		},
+		metadata: {
+			language: settings.defaultLocale,
+			title: settings.title,
+			subtitle: settings.tagline,
+			base: settings.url,
+			author: settings.author
+		}
+	});
 }
 
 export const config = baselineConfig;
