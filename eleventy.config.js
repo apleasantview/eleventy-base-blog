2d1e62479dec4275103a70591ca2625837027283
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-08-26 09:51:08 +0200

    Bring back the draft preprocessor

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,6 +4,17 @@ import settings from './src/_data/settings.js';
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default async function (eleventyConfig) {
 	await eleventyConfig.addPlugin(baseline(settings));
+
+	// Drafts, see also _data/eleventyDataSchema.js
+	eleventyConfig.addPreprocessor("drafts", "*", (data, content) => {
+		if (data.draft) {
+			data.title = `${data.title} (draft)`;
+		}
+
+		if(data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
+			return false;
+		}
+	});
 }
 
 export const config = baselineConfig;
